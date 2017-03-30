e4c46ece33ae4ae49910ba64f4d27624bd4ecd2d
Matt Runyon <M4tt-@users.noreply.github.com>
2017-03-30 10:33:54 -0400

    Updated gaussFit.m

--- gaussFit.m
+++ gaussFit.m
@@ -6,14 +6,14 @@ Description: This script is a function file that finds the gaussian
              one dimensional normal distribution.
 %}
 
-function [x stdX] = gaussFit(A)
+function [mu sigma] = gaussFit(A)
     x0 = 0;         % initialize x0 to zero
     x02 = 0;        % initialize x02 to zero
     for k = 1:length(A)
-        x0 = x0 + k*A(k)/sum(A);       % expectation value of x
-        x02 = x02 + (k^2)*A(k)/sum(A); % expectation value of x2
+        x0 = x0 + k.*A(k)./sum(A);       % expectation value of x
+        x02 = x02 + (k.^2).*A(k)./sum(A); % expectation value of x2
     end
     
-    sigma = sqrt(x02-x0^2);
+    sigma = sqrt(x02-x0.^2);
     mu = x0;
 end
